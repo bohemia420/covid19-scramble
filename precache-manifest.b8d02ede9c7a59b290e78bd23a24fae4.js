b02dbd3575c1dbb67f1f2cd5bbaa8b18567d0ec1
Bohemia420 <ksumeet40@gmail.com>
2020-05-29 00:18:01 +0530

    Updates

---
+++ precache-manifest.b8d02ede9c7a59b290e78bd23a24fae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a70d26020236fe7a80e1d5af52e3aa5",
+    "url": "/covid19-scramble/index.html"
+  },
+  {
+    "revision": "7ca35d66087b1ae48239",
+    "url": "/covid19-scramble/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2f0034ef66d311cc58f7",
+    "url": "/covid19-scramble/static/js/2.8e7bc9a8.chunk.js"
+  },
+  {
+    "revision": "7ca35d66087b1ae48239",
+    "url": "/covid19-scramble/static/js/main.be9184fb.chunk.js"
+  },
+  {
+    "revision": "6ec5795112ea2010cac1",
+    "url": "/covid19-scramble/static/js/runtime-main.0e907dbe.js"
+  },
+  {
+    "revision": "51a8486a87e96750b29e25c8a7ce3347",
+    "url": "/covid19-scramble/static/media/logo192.51a8486a.png"
+  }
+]);
